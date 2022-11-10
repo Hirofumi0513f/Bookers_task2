a359ff76a857dacf1475e7239a536dc27c3f9f36
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2022-11-10 23:31:38 +0000

    [Update]bookers_202211110830

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,8 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books'
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(book.id)
   end
 
   def index
@@ -24,6 +25,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book.id)
   end
 
