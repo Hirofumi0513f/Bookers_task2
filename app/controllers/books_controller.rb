3ade58fd87727f08e7dee205a448138f03c9c732
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2022-11-12 01:35:24 +0000

    [Update]bookers_202211121035

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,18 @@
 class BooksController < ApplicationController
-  def new
-    @Book = Book.new
+  def index
+    @books = Book.all.order(id: :asc)
+    @book =Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    flash[:notice] = "Book was successfully created."
-    redirect_to book_path(book.id)
-  end
-
-  def index
-    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all(id: :asc)
+      render :index
+    end
   end
 
   def show
